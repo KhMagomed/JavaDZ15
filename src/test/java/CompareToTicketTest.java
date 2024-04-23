b9d6ab8b24fd5f7bfece33854847dbe3691982a7
Neto test <xmag91@mail.ru>
2024-04-23 21:48:35 +0300

    Fixed1 commit

--- src/test/java/CompareToTicketTest.java
+++ src/test/java/CompareToTicketTest.java
@@ -57,5 +57,54 @@ public class CompareToTicketTest {
         Ticket[] actual = manager.searchAndSortBy("Став", "Мира", comparator);
 
         Assertions.assertArrayEquals(expected, actual);
+    } @Test
+    public void TestTimeComporator3() {
+
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket = new Ticket("Став", "Мира", 505, 10, 16);
+        Ticket ticket2 = new Ticket("Став2", "Мира2", 60, 12, 17);
+        Ticket ticket3 = new Ticket("Став", "Мира", 5055, 10, 18);
+        Ticket ticket4 = new Ticket("Став23", "Мира245", 630, 12, 17);
+        Ticket ticket5 = new Ticket("Став", "Мира", 50522, 10, 12);
+        Ticket ticket6 = new Ticket("Став25", "Мира24", 603, 12, 17);
+
+        manager.add(ticket);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+
+        Comparator<Ticket> comparator = new TicketTimeComparator();
+
+        Ticket[] expected = {};
+        Ticket[] actual = manager.searchAndSortBy("Став", "Мир", comparator);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void TestTimeComporator4() {
+
+        AviaSouls manager = new AviaSouls();
+        Ticket ticket = new Ticket("Став", "Мира", 505, 10, 16);
+        Ticket ticket2 = new Ticket("Став2", "Мира2", 60, 12, 17);
+        Ticket ticket3 = new Ticket("Став", "Мира", 5055, 10, 18);
+        Ticket ticket4 = new Ticket("Став23", "Мира245", 630, 12, 17);
+        Ticket ticket5 = new Ticket("Став", "Мира", 50522, 10, 12);
+        Ticket ticket6 = new Ticket("Став25", "Мира24", 603, 12, 17);
+
+        manager.add(ticket);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+
+        Comparator<Ticket> comparator = new TicketTimeComparator();
+
+        Ticket[] expected = {ticket6};
+        Ticket[] actual = manager.searchAndSortBy("Став25", "Мира24", comparator);
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
